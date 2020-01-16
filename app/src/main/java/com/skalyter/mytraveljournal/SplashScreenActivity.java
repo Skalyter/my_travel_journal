e5077f7cac89848740db9ef8f3335136272d186d
Tiberiu G <tiberiu.gaspar98@gmail.com>
2020-01-17 00:00:51 +0200

    add SplashScreenActivity and change colors

---
+++ app/src/main/java/com/skalyter/mytraveljournal/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.skalyter.mytraveljournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
