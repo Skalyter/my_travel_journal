eb5ee983cef14d0dc566e3b4f3e25dec53ddbb42
Tiberiu G <tiberiu.gaspar98@gmail.com>
2020-01-16 22:30:20 +0200

    Removed boilerplate code, rearranging and renaming the classes

--- app/src/main/java/com/skalyter/mytraveljournal/ui/about/AboutFragment.java
+++ app/src/main/java/com/skalyter/mytraveljournal/ui/about/AboutFragment.java
@@ -1,4 +1,4 @@
-package com.skalyter.mytraveljournal.ui.gallery;
+package com.skalyter.mytraveljournal.ui.about;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.skalyter.mytraveljournal.R;
 
-public class GalleryFragment extends Fragment {
+public class AboutFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private AboutViewModel aboutViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        aboutViewModel =
+                ViewModelProviders.of(this).get(AboutViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_about, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        aboutViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
